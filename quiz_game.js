2dbed4864324b6571202d83569ad804f3542b21c
eitan_bardella <xxeitanxx11@gmail.com>
2023-07-19 11:36:41 -0300

    Quiz game funcional

--- quiz_game.js
+++ quiz_game.js
@@ -2,17 +2,17 @@
 const preguntas = [
     {
         pregunta:"¿Cuál es la capital de Argentina?",
-        options : ["Ottawa ", "Buenos Aires ", "Francia ", "Europa "],
+        options : ["Ottawa", "Buenos Aires", "Francia", "Europa"],
         answer: 1
     },
     {
         pregunta:"¿Cuál es el valor de X? x + 2(2+5)=10",
-        options : ["0 ", "24 ", "-3 ", "-4 "],
+        options : ["0", "24", "-3", "-4"],
         answer: 3
     },
     {
         pregunta:"Cuantos paises hay en el mundo",
-        options : ["206 ", "200 ", "145 ", "205 "],
+        options : ["206", "200", "145", "205"],
         answer: 0
     },
 ]
@@ -23,13 +23,15 @@ let score = 0;
 //Funcion que dispare la pregunta inicial
 function ask_question(question) {
     let user_answer = prompt(question.pregunta + " " + question.options)
-    if (preguntas.indexOf(parseInt(user_answer)) === question.answer) {
+    if (user_answer === question.options[parseInt(question.answer)]) {
         score++;
         alert("Respuesta Correcta!! Muy bien 10")
+    
     } else {
         alert ("Respuesta Incorrecta!! Falta Estudio")
     }
 }
+
 //iterador entre preguntas
 preguntas.forEach(function(question) {
     ask_question(question);
